ce39153a8aa9d89b19ed090c07e360bbcf78e93d
Apirak Fakin <apirakfakin@gmail.com>
2024-05-13 13:47:18 +0700

    feat: add ex-7

--- ex-7.js
+++ ex-7.js
@@ -41,8 +41,15 @@ let orders = [
   },
 ];
 
-// Start coding here
-let isArray;
-let creditCardTypeOfBlindermann;
-let productQuantityOfJoannet;
-let totalPurchaseOfDary;
+let isArray = Array.isArray(orders);
+let creditCardTypeOfBlindermann = orders[2].creditCardType;
+orders[2].creditCardType = "visa";
+let productQuantityOfJoannet = orders[3].productQuantity
+let totalPurchaseOfDary = orders[1].productPrice * orders[1].productQuantity
+orders.shift()
+
+console.log("orders:", orders);
+console.log("isArray:", isArray);
+console.log("creditCardTypeOfBlindermann:", creditCardTypeOfBlindermann);
+console.log("productQuantityOfJoannet:", productQuantityOfJoannet);
+console.log("totalPurchaseOfDary:", totalPurchaseOfDary);
