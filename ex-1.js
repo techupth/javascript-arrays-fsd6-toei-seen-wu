883b1ebf1680077c11822c97b564895bf8a2ee93
Apirak Fakin <apirakfakin@gmail.com>
2024-05-13 13:10:22 +0700

    feat: add ex-1

--- ex-1.js
+++ ex-1.js
@@ -5,3 +5,11 @@ let blueColor;
 let fourthColor;
 let fifthColor;
 let tenthColor;
+
+blueColor = colors[2]
+fourthColor = colors[3]
+fifthColor = colors[4]
+tenthColor = colors[9]
+
+colors[9] = tenthColor;
+console.log(blueColor, fourthColor, fifthColor, tenthColor);
